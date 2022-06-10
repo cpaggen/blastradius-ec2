f01394e2172183ec7a3ef2ddf1a1e1c541c96d39
Chris Paggen <cpaggen@gmail.com>
2022-06-10 17:57:21 +0200

    rc0a

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -22,6 +22,44 @@ variable "FRANKFURTKEY" {
   # passed as env variable from Github encrypted secret in action workflow
 }
 
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "cpaggen-bucket-one"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aesbucket1" {
+  bucket = aws_s3_bucket.bucket1.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+#locking part
+
+resource "aws_dynamodb_table" "tf_remote_state_locking" {
+  hash_key = "LockID"
+  name     = "cpaggen-tfstate"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "cpaggen-terraform-state"
+    key            = "index"
+    dynamodb_table = "cpaggen-tfstate"
+    region         = var.AWS_REGION
+  }
+}
+
 resource "aws_vpc" "terraformvpc1" {
   cidr_block       = "10.10.0.0/16"
   instance_tenancy = "dedicated"
